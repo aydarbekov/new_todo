4ce6f1dd34e64226e92c9b2cacab5f80f6cd669b
akyl <akyl.aydarbekov@gmail.com>
2019-10-08 20:47:49 +0600

    deleted my base delete class

--- TODO_list/webapp/views/base_views.py
+++ TODO_list/webapp/views/base_views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.views.generic import TemplateView
 from django.views.generic.base import View
 
-
+#delete with checkers
 class DeleteView(TemplateView):
     model = None
     url = None
@@ -12,22 +12,3 @@ class DeleteView(TemplateView):
         delete = request.POST.getlist('del')
         self.model.objects.filter(pk__in=delete).delete()
         return redirect(self.url)
-
-
-class BaseDeleteView(TemplateView):
-    model = None
-    template_name = None
-    redirect_url = ''
-    podtv = True
-    def get(self, request, *args, **kwargs):
-        obj = get_object_or_404(self.model, pk=kwargs['pk'])
-        if self.podtv == True:
-            return render(request, self.template_name, context={'obj': obj})
-        elif self.podtv == False:
-            obj.delete()
-            return redirect(self.redirect_url)
-
-    def post(self, request, **kwargs):
-        obj = get_object_or_404(self.model, pk=kwargs['pk'])
-        obj.delete()
-        return redirect(self.redirect_url)

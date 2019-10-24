782169d68acc007caf69bf818691ecfa07e7c64e
akyl <akyl.aydarbekov@gmail.com>
2019-10-24 14:22:38 +0600

    added NEXT

--- TODO_list/accounts/views.py
+++ TODO_list/accounts/views.py
@@ -4,13 +4,15 @@ from django.shortcuts import render, redirect
 
 def login_view(request):
     context = {}
+    next = request.GET.get('next')
+    next_page = request.session.setdefault('next_page', next)
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('webapp:index')
+            return redirect(next_page)
         else:
             context['has_error'] = True
     return render(request, 'login.html', context=context)

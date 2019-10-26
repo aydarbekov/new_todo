c7e27d5fbcf677db1bf1c3222b86d0c7a372e5d6
akyl <akyl.aydarbekov@gmail.com>
2019-10-26 18:14:18 +0600

    Подключил базовый шаблон

--- TODO_list/accounts/urls.py
+++ TODO_list/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
+from accounts.views import register_view
+
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('create/', register_view, name='create')
 ]
 
 app_name = 'accounts'
---
+++ TODO_list/accounts/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from .forms import UserCreationForm
+
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('webapp:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'user_create.html', context={'form': form})
